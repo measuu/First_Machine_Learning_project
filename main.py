6ade51eb36cac55c9c4b7bf8991b303832170408
Dima <orlyk.do@gmail.com>
2026-06-29 20:20:47 +0200

    Analyse data

--- main.py
+++ main.py
@@ -3,34 +3,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
 from matplotlib.pyplot import bar
+import plotly.express as px
 
 #-----------------------------СТАРТ--------------------------------------
-
 df = pd.read_csv('data/covid_data.csv')
 
 # print(df.head(5), '\n')
 # print(df.info(),'\n')
-# print('Colums covid_data:\n')
+# print('Стовпці в covid_data:\n')
 # for i, item in enumerate(df):
 #     print(item, end="\t")
 #
 #     if (i + 1) % 10 == 0:
 #         print()
-
-
+#
+#
 numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
 for col in numeric_columns:
     df[col].fillna(df[col].mean())
-
-
-# print('\n', df.duplicated().sum())#0
-
-
+#
+# print('\n','Кількість дублікатів:', df.duplicated().sum())#0
+#
+# print('Среднє значення кількості випадків на міліон')
 # print(df['total_cases_per_million'].mean())
+# print('Медіана кількості випадків на міліон')
 # print(df['total_cases_per_million'].median())
+# print('Максимальне значення кількості випадків на міліон')
 # print(df['total_cases_per_million'].max())
-
-
+#
+#
 # df_brazil = df[df['location'] == 'Brazil']
 #
 # x = df_brazil['date']
@@ -41,7 +42,6 @@ for col in numeric_columns:
 # plt.xlabel("Дати")
 # plt.ylabel("Загальна кількість")
 # plt.show()
-#
 #
 # df_south_america = df[df['continent'] == 'South America']
 # df_europe = df[df['continent'] == 'Europe']
@@ -91,3 +91,96 @@ df['growth_rate_new_cases'] = df['growth_rate_new_cases'].fillna(0)
 df['growth_rate_new_deaths'] = df['growth_rate_new_deaths'].fillna(0)
 
 df.to_csv('data/cleaned_covid_data.csv', index=False)#Очищені дані збережено в cleaned_covid_data.csv
+
+
+cols = ['new_cases', 'new_deaths', 'total_cases', 'population', 'gdp_per_capita']
+cor = df[cols].corr()
+print(cor)
+
+plt.figure(figsize=(7, 5))
+sns.heatmap(cor, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Теплова карта')
+plt.tight_layout()
+plt.show()
+
+
+#Гістограми
+plt.figure(figsize=(10, 6))
+
+sns.histplot(data=df, x='total_cases', bins=30, kde=True)
+
+plt.title('Гістограми для всіх випадків')
+plt.xlabel('Кількість випадків')
+plt.ylabel('Частота')
+
+plt.show()
+
+
+plt.figure(figsize=(10, 6))
+
+sns.histplot(data=df, x='total_deaths', bins=30, kde=True)
+
+plt.title('Гістограми для всіх смертей')
+plt.xlabel('Кількість смертей')
+plt.ylabel('Частота')
+
+plt.show()
+
+
+#boxplot
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='continent', y='total_deaths_per_million', data=df)
+plt.xticks(rotation=45)
+plt.title('Розподіл смертей про міліон по континентам')
+plt.tight_layout()
+
+#pairplot
+sns.pairplot(df[['total_cases', 'total_deaths', 'total_vaccinations', 'population']].dropna())
+plt.show()
+
+#Аналіз трендів
+countries = ['United States', 'Ukraine', 'Germany']
+select = df[df['location'].isin(countries)]
+
+sns.lineplot(x='date', y='new_cases', hue='location', data=select)
+plt.title('Нові випадки в США, Україні та Німеччині')
+plt.show()
+
+
+plt.figure(figsize=(14,6))
+
+sns.lineplot(x='date', y='new_deaths', hue='location', data=select)
+plt.title('Нові смерті в США, Україні та Німеччині')
+plt.show()
+
+
+df_countries = df.copy()
+df_countries['date'] = pd.to_datetime(df_countries['date'], errors='coerce')
+latest_country = df_countries.sort_values('date').groupby('location').tail(1)
+
+top = latest_country.nlargest(10, 'total_cases_per_million')
+
+plt.figure(figsize=(12,6))
+sns.barplot(data=top, x='location', y='total_cases_per_million', palette='viridis')
+plt.title('Топ 10 країн за кількості всього випадків на міліон')
+plt.xlabel('Країни')
+plt.ylabel('Кількість випадків')
+
+plt.show()
+
+for country in countries:
+    country_data = df[df['location'] == country]
+
+    maxc = country_data.loc[country_data['new_cases'].idxmax()]
+    minc = country_data.loc[country_data['new_cases'].idxmin()]
+
+    maxd = country_data.loc[country_data['new_deaths'].idxmax()]
+    mind = country_data.loc[country_data['new_deaths'].idxmin()]
+
+    print(f'\n------{country}------')
+
+    print(f"Найбільше нових випадків: {maxc['new_cases']} ({maxc['date'].date()})")
+    print(f"Найменше нових випадків: {minc['new_cases']} ({minc['date'].date()})")
+
+    print(f"Найбільше нових смертей: {maxd['new_deaths']} ({maxd['date'].date()})")
+    print(f"Найменше нових смертей: {mind['new_deaths']} ({mind['date'].date()})")
